5ed9726656c8aee31b3dd7863c40f7661301d049
JB-Gra <timishayd@gmail.com>
2024-05-01 16:06:52 -0600

    Agrega funcionalidad de conversor de monedas en clase Principal que incluye: Menú con opciones de lista de divisa (Latam), conversor de monedas y salida del programa; modifica clase ConsultaMoneda y record Moneda para corrección de errores en clase Principal

--- src/ConsultaMoneda.java
+++ src/ConsultaMoneda.java
@@ -1,2 +1,25 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConsultaMoneda {
+  public Moneda buscaMoneda(String codigoBase, String codigoDestino, double cantidadAConvertir) {
+    URI direccion = URI.create("https://v6.exchangerate-api.com/v6/9aee1a5c942de583eb24651a/pair/" + codigoBase + "/" + codigoDestino + "/" + cantidadAConvertir);
+
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(direccion)
+        .build();
+
+    try {
+      HttpResponse<String> response = client
+          .send(request, HttpResponse.BodyHandlers.ofString());
+      return new Gson().fromJson(response.body(), Moneda.class);
+    } catch (Exception e) {
+      throw new RuntimeException("No encontré ningún dato");
+    }
+  }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -2,7 +2,93 @@ import java.util.Scanner;
 
 public class Principal {
   public static void main(String[] args) {
+    ConsultaMoneda consultaMoneda = new ConsultaMoneda();
     Scanner teclado = new Scanner(System.in);
-    System.out.println("Prueba");
+    int opcion = 0;
+
+    String divisaOrigen;
+    String divisaDestino;
+    double cantidad;
+
+    System.out.println("******************");
+    System.out.println("¡Bienvenido/a al conversor de monedas!");
+    System.out.println("******************\n");
+
+    String menu = """
+          1 ---> Lista de divisas (LATAM)
+          2 ---> Conversión de monedas
+          9 ---> Salir
+          """;
+
+    while (opcion != 9) {
+      System.out.println("Seleccione el número de la operación que desee realizar:");
+      System.out.println(menu);
+      opcion = teclado.nextInt();
+
+      switch (opcion) {
+        case 1:
+          String codigosLatam = """
+              Argentina === ARS
+              Brasil === BRL
+              Chile === CLP
+              Colombia === COP
+              México === MXN
+              Nicaragua === NIO
+              Paraguay === PYG
+              Perú === PEN
+              Uruguay === UYU\n
+              Para una lista más detallada, consulte la página https://www.exchangerate-api.com/docs/supported-currencies\n
+              """;
+          System.out.println(codigosLatam);
+          break;
+        case 2:
+          System.out.println("Escriba la clave de la divisa de origen");
+          divisaOrigen = teclado.next().toUpperCase();
+
+          if (divisaOrigen.length() != 3) {
+            System.out.println("La clave debe tener solo tres caracteres");
+            System.out.println("Escriba la clave de la divisa de origen");
+            divisaOrigen = teclado.next().toUpperCase();
+          }
+
+          System.out.println("Escriba la clave de la divisa destino");
+          divisaDestino = teclado.next().toUpperCase();
+
+          if (divisaDestino.length() != 3) {
+            System.out.println("La clave debe tener solo tres caracteres\n");
+            System.out.println("Escriba la clave de la divisa destino");
+            divisaDestino = teclado.next().toUpperCase();
+          }
+
+          System.out.println("Ingrese la cantidad a convertir");
+          cantidad = teclado.nextDouble();
+
+          Moneda moneda = consultaMoneda.buscaMoneda(divisaOrigen, divisaDestino, cantidad);
+
+          System.out.println("Su resultado es: " + cantidad + " " + divisaOrigen + " son " + moneda.conversion_result() + " " + divisaDestino + "\n");
+          break;
+        case 9:
+          System.out.println("******************");
+          System.out.println("¡Gracias por usar el conversor de monedas! Vuelva pronto :D");
+          System.out.println("******************\n");
+          break;
+        default:
+          System.out.println("Opción no válida. Intente otra vez\n");
+      }
+    }
   }
 }
+
+/*
+    try {
+      String codigoBase = "USD";
+      String codigoDestino = "MXN";
+      double cantidad = 100;
+
+      Moneda moneda = consultaMoneda.buscaMoneda(codigoBase, codigoDestino, cantidad);
+      System.out.println(moneda);
+      System.out.println("Resultado de la conversión de " + cantidad + " " + codigoBase + " a " + codigoDestino + ": " + moneda.conversion_result());
+    } catch(RuntimeException e) {
+      System.out.println(e.getMessage());
+    }
+*/
